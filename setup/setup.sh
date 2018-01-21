caea828faa882be638f20393ce8c6849940cdf67
asinha94 <anuraagsinha@hotmail.com>
2018-01-21 00:11:35 -0800

    moved configuration stuff to separate folder

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+# Install Deps
 go get github.com/mholt/caddy/caddy
 go get github.com/caddyserver/builds
 go get github.com/RATDistributedSystems/mux
+
+# Get the latest plugin file for us to modify
 wget https://raw.githubusercontent.com/mholt/caddy/master/caddyhttp/httpserver/plugin.go
 python add_plugin.py
 mv plugin.go $GOPATH/src/github.com/mholt/caddy/caddyhttp/httpserver/
-cd $GOPATH/src/github.com/mholt/caddy/caddy
-go run build.go
+
