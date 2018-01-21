bafa4c52137599bce5e8cfa10269810af748afb9
asinha94 <asinha94@gmail.com>
2018-01-20 22:04:28 -0800

    added separate handlers to handlers.go

--- handler.go
+++ handler.go
@@ -9,13 +9,56 @@ import (
 )
 
 func main() {
-	mux.HandleFunc("/test", TestHandler)
-	mux.HandleFunc("/login", LoginHandler)
+	mux.HandleFunc("/add", AddHandler)
+	mux.HandleFunc("/buy", BuyHandler)
+	mux.HandleFunc("/sell", SellHandler)
+	mux.HandleFunc("/quote", QuoteHandler)
+	mux.HandleFunc("/summary", SummaryHandler)
+	mux.HandleFunc("/buytrigger", BuyTriggerHandler)
+	mux.HandleFunc("/selltrigger", SellTriggerHandler)
+	mux.HandleFunc("/canceltrigger", CancelTriggerHandler)
 	caddymain.Run()
 }
 
+func AddHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func BuyHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func SellHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func QuoteHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func SummaryHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func BuyTriggerHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func SellTriggerHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func CancelTriggerHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
 func LoginHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("%+v\n", r)
+	r.ParseForm()
+	uname_form := r.PostForm["uname"]
+	if uname_form != nil {
+		fmt.Println(uname_form[0])
+	}
+
 }
 
 func TestHandler(w http.ResponseWriter, r *http.Request) {
