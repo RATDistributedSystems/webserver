607d9240167f381f4175f8860487e81ed0623f62
asinha94 <asinha94@gmail.com>
2018-02-11 17:25:35 -0800

    renamed server and fixed dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM scratch
 
-COPY frontend server tserver.json Caddyfile /app/
+COPY frontend server config.json /app/
 WORKDIR "/app"
 EXPOSE 44440
-CMD ["./server"]
+CMD ["./webserver"]
--- setup/setup-docker-image.sh
+++ setup/setup-docker-image.sh
@@ -5,7 +5,7 @@ cd ..
 git clone https://github.com/RATDistributedSystems/frontend
 
 # Build our app in docker with CGO disabled (static linking) then copy it back outside
-docker run --rm -it -v "$GOPATH":/gopath -v "$(pwd)":/app -e "GOPATH=/gopath" -w /app golang:1.9 sh -c 'CGO_ENABLED=0 go build -a --installsuffix cgo --ldflags="-s" -o server'
+docker run --rm -it -v "$GOPATH":/gopath -v "$(pwd)":/app -e "GOPATH=/gopath" -w /app golang:1.9 sh -c 'CGO_ENABLED=0 go build -a --installsuffix cgo --ldflags="-s" -o webserver'
 
 # Build the image
 docker build -t ratwebserver .
